97d56f5e40972e124836daea659ca43222eaed50
bparent11 <benjamin.parent@etu.minesparis.psl.eu>
2026-01-18 19:22:56 +0100

    Added get_dict() and get_dataframe() methods.

--- src/bankparse/table_manager/base_table.py
+++ src/bankparse/table_manager/base_table.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import pandas as pd
 
 class Table(ABC):
     def __init__(self):
@@ -6,18 +7,28 @@ class Table(ABC):
         self.accountId = None
         self.owner = None
         self.extraction_date = None
+        self.content = None
 
     @abstractmethod
     def mergeTransactionLabel(self):
         pass
 
-    @abstractmethod
     def get_dict(self):
-        pass
+        output = {}
+        keys = self.content[0]
+        values = self.content[1:]
 
-    @abstractmethod
+        for i, key in enumerate(keys):
+            output[key] = [val[i] for val in values]
+
+        return output
+    
     def get_dataframe(self):
-        pass
+        output = pd.DataFrame(
+            data=self.get_dict()
+        )
+
+        return output
 
 class BankTransactionTable(Table):
     def __init__(self):
